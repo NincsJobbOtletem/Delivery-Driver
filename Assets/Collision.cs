b38304a6161d05902d60ac91c3f766de424f3b2f
NincsJobbOtletem <patrikvok123312@gmail.com>
2022-08-23 18:00:43 +0200

    map working need camera follow

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     // utkozes
-    void OnCollisionEnter2D(Collision2D other) {
-        sadsa
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("utkozoss van");
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("trigger");
     }
 }
